c049e8d3327875c003e6eb0b2a69cc26db5c9d66
Jason Nutt <jnutt367@gmail.com>
2021-10-10 17:06:53 -0400

    commit for gsap animates dining experience

--- js/dinner.js
+++ js/dinner.js
@@ -1,1 +1,8 @@
 gsap.from("svg", { opacity: 0, duration: 3, ease: "bounce" })
+
+let tl = gsap.timeline()
+
+//let doubleBoxLift = gsap.timeline(  )
+.to(".wine", { x: -50, y: -210, duration: 3 })
+    .to(".dine", { x: 50, y: -240, duration: 2 })
+    .to(".special-times", { x: -50, y: -700, ease: "elastic" })
