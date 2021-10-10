265950e9bee4ab0af56b3bdd646d8bc60f033632
Jason Nutt <jnutt367@gmail.com>
2021-10-10 17:16:02 -0400

    commit for longer wait in between tweeners

--- js/dinner.js
+++ js/dinner.js
@@ -4,5 +4,5 @@ let tl = gsap.timeline()
 
 //let doubleBoxLift = gsap.timeline(  )
 .to(".wine", { x: -50, y: -210, duration: 3 })
-    .to(".dine", { x: 50, y: -240, duration: 2 })
-    .to(".special-times", { x: -50, y: -700, ease: "elastic" })
+    .to(".dine", { x: 50, y: -240, duration: 2 }, 5)
+    .to(".special-times", { x: -50, y: -700, duration: 5, ease: "elastic" }, 7)
