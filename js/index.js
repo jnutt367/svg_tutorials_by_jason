95b9ef61d06526c7c155359f663595098b919f17
Jason Nutt <jnutt367@gmail.com>
2021-10-12 17:21:24 -0400

    commit for media query for smaller screens

--- js/index.js
+++ js/index.js
@@ -3,13 +3,24 @@ const sections = gsap.utils.toArray(".cols-2")
 sections.forEach((element, index) => {
     let items = element.querySelectorAll(".item")
     let distance = (index % 2 == 0) ? -100 : 100
-    let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: element,
-                start: "top 80%",
-                toggleActions: "restart none none reverse"
-            }
-        })
-        .from(items, { xPercent: gsap.utils.wrap([distance, -distance]), duration: 2 })
-        .fromTo(".main", { opacity: 0, duration: 2, ease: "back" }, { opacity: 1 }, 0.2)
+    let tl = gsap.timeline({ paused: true })
+
+
+    tl.from(items, { xPercent: gsap.utils.wrap([distance, -distance]), duration: 2 })
+    tl.from(items, { opacity: 0, duration: 0.3 }, 0)
+
+    ScrollTrigger.create({
+        trigger: element,
+        start: "top 80%",
+        markers: true,
+        onEnter: () => tl.play()
+    })
+
+    ScrollTrigger.create({
+        trigger: element,
+        start: "top 100%",
+        markers: true,
+        onLeaveBack: () => tl.pause(0)
+    })
+
 })
