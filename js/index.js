7d034b2b29d084d4abce099ca4b0bf880281cbf7
Jason Nutt <jnutt367@gmail.com>
2021-10-12 18:41:46 -0400

    commit for mobile animations

--- js/index.js
+++ js/index.js
@@ -1,26 +1,74 @@
+gsap.from(".h2", { opacity: 0, scale: 2, duration: 2 }, 2)
 const sections = gsap.utils.toArray(".cols-2")
-
-sections.forEach((element, index) => {
-    let items = element.querySelectorAll(".item")
-    let distance = (index % 2 == 0) ? -100 : 100
-    let tl = gsap.timeline({ paused: true })
+    //important to do this BEFORE ScrollTriggers are created
+ScrollTrigger.saveStyles(".item, .item div")
 
 
-    tl.from(items, { xPercent: gsap.utils.wrap([distance, -distance]), duration: 2 })
-    tl.from(items, { opacity: 0, duration: 0.3 }, 0)
+ScrollTrigger.matchMedia({
 
-    ScrollTrigger.create({
-        trigger: element,
-        start: "top 80%",
-        markers: true,
-        onEnter: () => tl.play()
-    })
+    // desktop
+    "(min-width: 601px)": function() {
+        sections.forEach((element, index) => {
+            let items = element.querySelectorAll(".item")
+            let distance = (index % 2 == 0) ? 100 : -100
+            let tl = gsap.timeline({ paused: true })
 
-    ScrollTrigger.create({
-        trigger: element,
-        start: "top 100%",
-        markers: true,
-        onLeaveBack: () => tl.pause(0)
-    })
+            tl.from(items, { duration: 0.5, xPercent: gsap.utils.wrap([-distance, distance]) })
+            tl.from(items, { opacity: 0, duration: 0.3 }, 0)
 
-})
+            ScrollTrigger.create({
+
+                trigger: element,
+                start: "top 80%",
+                onEnter: () => tl.play()
+            })
+
+            ScrollTrigger.create({
+                trigger: element,
+                start: "top 100%",
+                onLeaveBack: () => tl.pause(0)
+            })
+        })
+
+
+    }
+
+    ,
+
+    // small screen
+    "(max-width: 600px)": function() {
+
+        // Any ScrollTriggers created inside these functions are segregated and get
+        // reverted/killed when the media query doesn't match anymore. 
+        gsap.utils.toArray(".item").forEach((element, index) => {
+
+            let target = element.querySelector("div")
+
+            let tl = gsap.timeline({ paused: true })
+            if (target.classList.contains("photo")) {
+                tl.from(target, { duration: 0.5, opacity: 0, scale: 0.8 })
+            } else {
+                tl.from(target, { duration: 0.5, opacity: 0, y: 100 })
+            }
+
+            ScrollTrigger.create({
+
+                trigger: element,
+                start: "top 90%",
+                onEnter: () => tl.play()
+            })
+
+            ScrollTrigger.create({
+                trigger: element,
+                start: "top 100%",
+                onLeaveBack: () => tl.pause(0)
+            })
+
+
+
+        })
+    }
+
+
+
+});
